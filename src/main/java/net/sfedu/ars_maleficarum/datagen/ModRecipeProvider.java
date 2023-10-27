742174964559e4143b23bcd2631e1bd3207e4466
L-S-N <114927042+L-S-N@users.noreply.github.com>
2023-10-27 22:55:31 +0300

    Добавлены (не окончательные) рецепты всех детекторов руд

--- src/main/java/net/sfedu/ars_maleficarum/datagen/ModRecipeProvider.java
+++ src/main/java/net/sfedu/ars_maleficarum/datagen/ModRecipeProvider.java
@@ -3,14 +3,17 @@ package net.sfedu.ars_maleficarum.datagen;
 import net.minecraft.data.PackOutput;
 import net.minecraft.data.recipes.*;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.Items;
 import net.minecraft.world.item.crafting.AbstractCookingRecipe;
 import net.minecraft.world.item.crafting.Ingredient;
 import net.minecraft.world.item.crafting.RecipeSerializer;
 import net.minecraft.world.level.ItemLike;
+import net.minecraft.world.level.block.Blocks;
 import net.minecraftforge.common.crafting.conditions.IConditionBuilder;
 import net.sfedu.ars_maleficarum.ArsMaleficarum;
 import net.sfedu.ars_maleficarum.block.ModBlocks;
 import net.sfedu.ars_maleficarum.item.ModItems;
+import net.sfedu.ars_maleficarum.util.ModTags;
 
 import java.util.List;
 import java.util.function.Consumer;
@@ -83,6 +86,49 @@ public class ModRecipeProvider extends RecipeProvider implements IConditionBuild
                 .requires(ModItems.CURSED_GOLD.get())
                 .unlockedBy(getHasName(ModItems.CURSED_GOLD.get()),has(ModItems.CURSED_GOLD.get()))
                 .save(pWriter);
+        //Крафт детектора углеродной руды
+        ShapedRecipeBuilder.shaped(RecipeCategory.MISC, ModItems.CARBON_DETECTOR.get())
+                .pattern("#*#")
+                .pattern("* *")
+                .pattern("#*#")
+                .define('#', Blocks.GLASS)
+                .define('*', Items.COAL)
+                .unlockedBy(getHasName(Blocks.GLASS),has(Blocks.GLASS))
+                .unlockedBy(getHasName(Items.COAL),has(Items.COAL))
+                .save(pWriter);
+        //Крафт детектора металлической руды
+        ShapedRecipeBuilder.shaped(RecipeCategory.MISC, ModItems.METAL_DETECTOR.get())
+                .pattern("#*#")
+                .pattern("@ $")
+                .pattern("#|#")
+                .define('#', Items.COPPER_INGOT)
+                .define('*', ModItems.CURSED_GOLD.get())
+                .define('@', Items.GOLD_INGOT)
+                .define('|', ModItems.SILVER.get())
+                .define('$', Items.IRON_INGOT)
+                .unlockedBy(getHasName(Items.COPPER_INGOT),has(Items.COPPER_INGOT))
+                .unlockedBy(getHasName(Items.IRON_INGOT),has(Items.IRON_INGOT))
+                .unlockedBy(getHasName(Items.GOLD_INGOT),has(Items.GOLD_INGOT))
+                .unlockedBy(getHasName(ModItems.SILVER.get()),has(ModItems.SILVER.get()))
+                .unlockedBy(getHasName(ModItems.CURSED_GOLD.get()),has(ModItems.CURSED_GOLD.get()))
+                .save(pWriter);
+        //Крафт детектора ценной руды
+        ShapedRecipeBuilder.shaped(RecipeCategory.MISC, ModItems.VALUABLE_DETECTOR.get())
+                .pattern("#*#")
+                .pattern("@ $")
+                .pattern("#|#")
+                .define('#', Items.QUARTZ)
+                .define('*', Items.AMETHYST_SHARD)
+                .define('@', Items.EMERALD)
+                .define('|', Items.REDSTONE)
+                .define('$', Items.LAPIS_LAZULI)
+                .unlockedBy(getHasName(Items.QUARTZ),has(Items.QUARTZ))
+                .unlockedBy(getHasName(Items.AMETHYST_SHARD),has(Items.AMETHYST_SHARD))
+                .unlockedBy(getHasName(Items.EMERALD),has(Items.EMERALD))
+                .unlockedBy(getHasName(Items.REDSTONE),has(Items.REDSTONE))
+                .unlockedBy(getHasName(Items.LAPIS_LAZULI),has(Items.LAPIS_LAZULI))
+                .save(pWriter);
+
     }
     //Генерация .json файлов для блоков, которые могут быть переплавлены
     protected static void oreSmelting(Consumer<FinishedRecipe> pFinishedRecipeConsumer, List<ItemLike> pIngredients, RecipeCategory pCategory, ItemLike pResult, float pExperience, int pCookingTIme, String pGroup) {
