658cb046c4e942ea8c8f95650214afac78d461a5
L-S-N <114927042+L-S-N@users.noreply.github.com>
2023-11-10 21:09:51 +0300

    Added drop from moonlight flower

--- src/main/java/net/sfedu/ars_maleficarum/datagen/loot/ModBlockLootTables.java
+++ src/main/java/net/sfedu/ars_maleficarum/datagen/loot/ModBlockLootTables.java
@@ -7,6 +7,7 @@ import net.minecraft.world.item.Item;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.item.enchantment.Enchantments;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.storage.loot.LootPool;
 import net.minecraft.world.level.storage.loot.LootTable;
 import net.minecraft.world.level.storage.loot.entries.LootItem;
@@ -41,6 +42,7 @@ public class ModBlockLootTables extends BlockLootSubProvider {
     //Генерация кастомного лута с блоков
     protected void generate() {
         generatedSunlight_flower_Drop();
+        generatedMoonlight_flower_Drop();
         generateSageCropDrop();
         generateMarigoldCropDrop();
         //Блоки, которые при ломании дропают сами себя
@@ -62,7 +64,6 @@ public class ModBlockLootTables extends BlockLootSubProvider {
 
 
 
-
     }
     //Вынесенная отдельно процедура регистрации выпадения предметов при сборе культуры солнечный свет
     protected void generatedSunlight_flower_Drop(){
@@ -72,6 +73,15 @@ public class ModBlockLootTables extends BlockLootSubProvider {
         //Выпадение предметов при сборе созревшей культуры
         this.add(ModBlocks.SUNLIGHT_FLOWER_CROP.get(), createCropDropsWith2Items_SecondItemRare(ModBlocks.SUNLIGHT_FLOWER_CROP.get(),ModItems.SUNLIGHT_FLOWER.get(),Items.GLOWSTONE_DUST,
                 ModItems.SUNLIGHT_FLOWER_SEED.get(),lootitemcondition$builder2));
+    }
+    //Вынесенная отдельно процедура регистрации выпадения предметов при сборе культуры лунный свет
+    protected void generatedMoonlight_flower_Drop(){
+        LootItemCondition.Builder lootitemcondition$builder2 = LootItemBlockStatePropertyCondition
+                .hasBlockStateProperties(ModBlocks.MOONLIGHT_FLOWER_CROP.get())
+                .setProperties(StatePropertiesPredicate.Builder.properties().hasProperty(SunlightFlower.AGE, 6));
+        //Выпадение предметов при сборе созревшей культуры
+        this.add(ModBlocks.MOONLIGHT_FLOWER_CROP.get(), createCropDropsWith2Items(ModBlocks.MOONLIGHT_FLOWER_CROP.get(),ModItems.MOONLIGHT_FLOWER.get(),ModItems.MOONLIGHT_FLOWER_SEED.get(),
+                ModItems.MOONLIGHT_FLOWER_SEED.get(),lootitemcondition$builder2,0.08F,1));
     }
     //Реализация возможности пройтись циклом по всем блокам (вроде бы??)
     protected Iterable<Block> getKnownBlocks() {
@@ -87,7 +97,7 @@ public class ModBlockLootTables extends BlockLootSubProvider {
 
         //Выпадение предметов при сборе созревшей культуры
         this.add(ModBlocks.SAGE_CROP.get(),createCropDropsWith2Items(ModBlocks.SAGE_CROP.get(), ModItems.SAGE_FLOWER.get(), ModItems.SAGE_LEAF.get(),
-                ModItems.SAGE_SEED.get(), lootitemcondition$builder));
+                ModItems.SAGE_SEED.get(), lootitemcondition$builder,0.53247F,3));
 
     }
 
@@ -104,10 +114,10 @@ public class ModBlockLootTables extends BlockLootSubProvider {
     }
 
     //Дополнительная функция дропа, которой можно пользоваться, если нужно, чтобы с созревшей культуры падало 2 предмета, помимо семян.
-    protected LootTable.Builder createCropDropsWith2Items(Block pCropBlock, Item pGrownCropItem1,Item pGrownCropItem2, Item pSeedsItem, LootItemCondition.Builder pDropGrownCropCondition) {
-        return this.applyExplosionDecay(pCropBlock, LootTable.lootTable().withPool(LootPool.lootPool().add(LootItem.lootTableItem(pGrownCropItem1).when(pDropGrownCropCondition).otherwise(LootItem.lootTableItem(pSeedsItem)))).withPool(LootPool.lootPool().add(LootItem.lootTableItem(pGrownCropItem2).when(pDropGrownCropCondition))).withPool(LootPool.lootPool().when(pDropGrownCropCondition).add(LootItem.lootTableItem(pSeedsItem).apply(ApplyBonusCount.addBonusBinomialDistributionCount(Enchantments.BLOCK_FORTUNE, 0.5714286F, 3)))));
+    protected LootTable.Builder createCropDropsWith2Items(Block pCropBlock, Item pGrownCropItem1,Item pGrownCropItem2, Item pSeedsItem, LootItemCondition.Builder pDropGrownCropCondition, float probability, int extra_items) {
+        return this.applyExplosionDecay(pCropBlock, LootTable.lootTable().withPool(LootPool.lootPool().add(LootItem.lootTableItem(pGrownCropItem1).when(pDropGrownCropCondition).otherwise(LootItem.lootTableItem(pSeedsItem)))).withPool(LootPool.lootPool().add(LootItem.lootTableItem(pGrownCropItem2).when(pDropGrownCropCondition).apply(SetItemCountFunction.setCount(UniformGenerator.between(0,1))))).withPool(LootPool.lootPool().when(pDropGrownCropCondition).add(LootItem.lootTableItem(pSeedsItem).apply(ApplyBonusCount.addBonusBinomialDistributionCount(Enchantments.BLOCK_FORTUNE, probability, extra_items)))));
     }
-    //Дополнительная функция дропа, которой можно пользоваться, если нужно, чтобы какой-то предмет дропаляс редко
+    //Дополнительная функция дропа, которой можно пользоваться, если нужно, чтобы какой-то предмет дропался редко
     //Первый предмет - обычный, второй предмет - редкий, третий - семена
     protected LootTable.Builder createCropDropsWith2Items_SecondItemRare(Block pCropBlock, Item pGrownCropItem1,Item pGrownCropItem2, Item pSeedsItem, LootItemCondition.Builder pDropGrownCropCondition) {
         return this.applyExplosionDecay(pCropBlock, LootTable.lootTable().withPool(LootPool.lootPool().add(LootItem.lootTableItem(pGrownCropItem1).when(pDropGrownCropCondition).otherwise(LootItem.lootTableItem(pSeedsItem)))).withPool(LootPool.lootPool().when(pDropGrownCropCondition).add(LootItem.lootTableItem(pGrownCropItem2).apply(SetItemCountFunction.setCount(UniformGenerator.between(0F, 0F))).apply(ApplyBonusCount.addBonusBinomialDistributionCount(Enchantments.BLOCK_FORTUNE, 0.1234F, 1)))).withPool(LootPool.lootPool().when(pDropGrownCropCondition).add(LootItem.lootTableItem(pSeedsItem).apply(ApplyBonusCount.addBonusBinomialDistributionCount(Enchantments.BLOCK_FORTUNE, 0.5261F, 3)))));
