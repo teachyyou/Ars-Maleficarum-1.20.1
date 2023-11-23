6e369d9758aa40ddbf374b5e5c2a0ff1e9e6c246
Игорь Кудрявцев <kudriavtsiev_2004@mail.ru>
2023-11-24 01:59:58 +0300

    Added second creative tab with new ingredient items

---
+++ src/main/java/net/sfedu/ars_maleficarum/item/custom/IngredientItem.java
@@ -0,0 +1,9 @@
+package net.sfedu.ars_maleficarum.item.custom;
+
+import net.minecraft.world.item.Item;
+
+public class IngredientItem extends Item {
+    public IngredientItem(Properties pProperties) {
+        super(pProperties);
+    }
+}
