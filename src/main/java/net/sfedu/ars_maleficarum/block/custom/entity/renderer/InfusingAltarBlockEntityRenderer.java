b85e0f17b9f70bad45c29305059d16ab17e14a3a
Игорь Кудрявцев <kudriavtsiev_2004@mail.ru>
2023-12-02 23:20:02 +0300

    Fixed Infusing Altar items render issue

--- src/main/java/net/sfedu/ars_maleficarum/block/custom/entity/renderer/InfusingAltarBlockEntityRenderer.java
+++ src/main/java/net/sfedu/ars_maleficarum/block/custom/entity/renderer/InfusingAltarBlockEntityRenderer.java
@@ -10,6 +10,7 @@ import net.minecraft.client.renderer.blockentity.BlockEntityRendererProvider;
 import net.minecraft.client.renderer.entity.ItemRenderer;
 import net.minecraft.client.renderer.texture.OverlayTexture;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemDisplayContext;
 import net.minecraft.world.item.ItemStack;
@@ -17,6 +18,7 @@ import net.minecraft.world.item.Items;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.LightLayer;
 import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.HorizontalDirectionalBlock;
 import net.sfedu.ars_maleficarum.block.custom.InfusingAltarBlock;
 import net.sfedu.ars_maleficarum.block.custom.entity.InfusingAltarBlockEntity;
 
@@ -31,69 +33,42 @@ public class InfusingAltarBlockEntityRenderer implements BlockEntityRenderer<Inf
     public void render(InfusingAltarBlockEntity pBlockEntity, float pPartialTick, PoseStack pPoseStack, MultiBufferSource pBuffer,
                        int pPackedLight, int pPackedOverlay) {
 
-
+        Direction d = pBlockEntity.getBlockState().getValue(HorizontalDirectionalBlock.FACING);
         ItemRenderer itemRenderer = Minecraft.getInstance().getItemRenderer();
-        //List<ItemStack> items = List.of(ItemStack.EMPTY,ItemStack.EMPTY,ItemStack.EMPTY,ItemStack.EMPTY, ItemStack.EMPTY,ItemStack.EMPTY);
         List<ItemStack> items = pBlockEntity.getRenderStack();
-        pPoseStack.pushPose();
-        pPoseStack.translate(0.25f,0.575f,0.75f);
-        pPoseStack.scale(0.3f,0.3f,0.3f);
-        pPoseStack.mulPose(Axis.YN.rotationDegrees(pBlockEntity.getBlockState().getValue(InfusingAltarBlock.FACING).toYRot()));
-        pPoseStack.mulPose(Axis.XP.rotationDegrees(270));
+        List<Float> xCords = List.of(0.25f,0.25f,0.65f,0.85f,0.65f,0.5f);
+        List<Float> zCords = List.of(0.75f,0.25f,0.225f,0.5f,0.775f,0.5f);
+        if (d==Direction.EAST || d==Direction.WEST) {
+            for (int i = 0; i < 6; i++) {
+                float x = d==Direction.WEST ? (1-xCords.get(i)) : (xCords.get(i));
+                float z = d==Direction.WEST ? (1-zCords.get(i)) : (zCords.get(i));
+                pPoseStack.pushPose();
+                pPoseStack.translate(x,0.575f,z);
+                pPoseStack.scale(0.3f,0.3f,0.3f);
+                pPoseStack.mulPose(Axis.YN.rotationDegrees(pBlockEntity.getBlockState().getValue(InfusingAltarBlock.FACING).toYRot()));
+                pPoseStack.mulPose(Axis.XP.rotationDegrees(270));
 
-        itemRenderer.renderStatic(items.get(0), ItemDisplayContext.FIXED,getLightLevel(pBlockEntity.getLevel(),
-                pBlockEntity.getBlockPos()), OverlayTexture.NO_OVERLAY,pPoseStack,pBuffer,pBlockEntity.getLevel(),1);
-        pPoseStack.popPose();
+                itemRenderer.renderStatic(items.get(0), ItemDisplayContext.FIXED,getLightLevel(pBlockEntity.getLevel(),
+                        pBlockEntity.getBlockPos()), OverlayTexture.NO_OVERLAY,pPoseStack,pBuffer,pBlockEntity.getLevel(),1);
+                pPoseStack.popPose();
+            }
+        }
+        else if (d==Direction.NORTH || d== Direction.SOUTH) {
+            for (int i = 0; i < 6; i++) {
+        /*z*/   float z = d==Direction.SOUTH ? (xCords.get(i)) : (1-xCords.get(i));
+        /*x*/   float x = d==Direction.SOUTH ? (1-zCords.get(i)) : (zCords.get(i));
+                pPoseStack.pushPose();
+                pPoseStack.translate(x,0.575f,z);
+                pPoseStack.scale(0.3f,0.3f,0.3f);
+                pPoseStack.mulPose(Axis.YN.rotationDegrees(pBlockEntity.getBlockState().getValue(InfusingAltarBlock.FACING).toYRot()));
+                pPoseStack.mulPose(Axis.XP.rotationDegrees(270));
 
-        pPoseStack.pushPose();
-        pPoseStack.translate(0.25f,0.575f,0.25f);
-        pPoseStack.scale(0.3f,0.3f,0.3f);
-        pPoseStack.mulPose(Axis.YN.rotationDegrees(pBlockEntity.getBlockState().getValue(InfusingAltarBlock.FACING).toYRot()));
-        pPoseStack.mulPose(Axis.XP.rotationDegrees(270));
+                itemRenderer.renderStatic(items.get(0), ItemDisplayContext.FIXED,getLightLevel(pBlockEntity.getLevel(),
+                        pBlockEntity.getBlockPos()), OverlayTexture.NO_OVERLAY,pPoseStack,pBuffer,pBlockEntity.getLevel(),1);
+                pPoseStack.popPose();
+            }
+        }
 
-        itemRenderer.renderStatic(items.get(1), ItemDisplayContext.FIXED,getLightLevel(pBlockEntity.getLevel(),
-                pBlockEntity.getBlockPos()), OverlayTexture.NO_OVERLAY,pPoseStack,pBuffer,pBlockEntity.getLevel(),1);
-        pPoseStack.popPose();
-
-        pPoseStack.pushPose();
-        pPoseStack.translate(0.65f,0.575f,0.225f);
-        pPoseStack.scale(0.3f,0.3f,0.3f);
-        pPoseStack.mulPose(Axis.YN.rotationDegrees(pBlockEntity.getBlockState().getValue(InfusingAltarBlock.FACING).toYRot()));
-        pPoseStack.mulPose(Axis.XP.rotationDegrees(270));
-
-        itemRenderer.renderStatic(items.get(2), ItemDisplayContext.FIXED,getLightLevel(pBlockEntity.getLevel(),
-                pBlockEntity.getBlockPos()), OverlayTexture.NO_OVERLAY,pPoseStack,pBuffer,pBlockEntity.getLevel(),1);
-        pPoseStack.popPose();
-
-        pPoseStack.pushPose();
-        pPoseStack.translate(0.85f,0.575f,0.5f);
-        pPoseStack.scale(0.3f,0.3f,0.3f);
-        pPoseStack.mulPose(Axis.YN.rotationDegrees(pBlockEntity.getBlockState().getValue(InfusingAltarBlock.FACING).toYRot()));
-        pPoseStack.mulPose(Axis.XP.rotationDegrees(270));
-
-        itemRenderer.renderStatic(items.get(3), ItemDisplayContext.FIXED,getLightLevel(pBlockEntity.getLevel(),
-                pBlockEntity.getBlockPos()), OverlayTexture.NO_OVERLAY,pPoseStack,pBuffer,pBlockEntity.getLevel(),1);
-        pPoseStack.popPose();
-
-        pPoseStack.pushPose();
-        pPoseStack.translate(0.65f,0.575f,0.775f);
-        pPoseStack.scale(0.3f,0.3f,0.3f);
-        pPoseStack.mulPose(Axis.YN.rotationDegrees(pBlockEntity.getBlockState().getValue(InfusingAltarBlock.FACING).toYRot()));
-        pPoseStack.mulPose(Axis.XP.rotationDegrees(270));
-
-        itemRenderer.renderStatic(items.get(4), ItemDisplayContext.FIXED,getLightLevel(pBlockEntity.getLevel(),
-                pBlockEntity.getBlockPos()), OverlayTexture.NO_OVERLAY,pPoseStack,pBuffer,pBlockEntity.getLevel(),1);
-        pPoseStack.popPose();
-
-        pPoseStack.pushPose();
-        pPoseStack.translate(0.5f,0.575f,0.5f);
-        pPoseStack.scale(0.3f,0.3f,0.3f);
-        pPoseStack.mulPose(Axis.YN.rotationDegrees(pBlockEntity.getBlockState().getValue(InfusingAltarBlock.FACING).toYRot()));
-        pPoseStack.mulPose(Axis.XP.rotationDegrees(270));
-
-        itemRenderer.renderStatic(items.get(5), ItemDisplayContext.FIXED,getLightLevel(pBlockEntity.getLevel(),
-                pBlockEntity.getBlockPos()), OverlayTexture.NO_OVERLAY,pPoseStack,pBuffer,pBlockEntity.getLevel(),1);
-        pPoseStack.popPose();
 
 
     }
