f9f5d1160a7ef2279fbcb3acdafa833a59103e83
L-S-N <114927042+L-S-N@users.noreply.github.com>
2024-01-31 14:29:57 +0300

    Add interface effects to custom crafts

--- src/main/java/net/sfedu/ars_maleficarum/compat/OdourExtractingFurnaceRecipeCategory.java
+++ src/main/java/net/sfedu/ars_maleficarum/compat/OdourExtractingFurnaceRecipeCategory.java
@@ -1,13 +1,26 @@
 package net.sfedu.ars_maleficarum.compat;
-
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
+import com.mojang.math.Constants;
+import mezz.jei.api.gui.drawable.IDrawableAnimated;
+import mezz.jei.api.gui.drawable.IDrawableStatic;
+import mezz.jei.api.helpers.IGuiHelper;
+import mezz.jei.api.recipe.category.IRecipeCategory;
 import mezz.jei.api.constants.VanillaTypes;
 import mezz.jei.api.gui.builder.IRecipeLayoutBuilder;
 import mezz.jei.api.gui.drawable.IDrawable;
+
+import mezz.jei.api.gui.drawable.IDrawableAnimated;
+import mezz.jei.api.gui.ingredient.IRecipeSlotsView;
 import mezz.jei.api.helpers.IGuiHelper;
 import mezz.jei.api.recipe.IFocusGroup;
 import mezz.jei.api.recipe.RecipeIngredientRole;
 import mezz.jei.api.recipe.RecipeType;
 import mezz.jei.api.recipe.category.IRecipeCategory;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.Font;
+import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.Item;
@@ -19,7 +32,6 @@ import net.sfedu.ars_maleficarum.item.ModItems;
 import net.sfedu.ars_maleficarum.recipe.OdourExtractingRecipe;
 
 import java.util.List;
-
 public class OdourExtractingFurnaceRecipeCategory implements IRecipeCategory<OdourExtractingRecipe> {
     public static final ResourceLocation UID = new ResourceLocation(ArsMaleficarum.MOD_ID,"odour_extracting");
     public static final ResourceLocation TEXTURES = new ResourceLocation(ArsMaleficarum.MOD_ID,"textures/gui/odour_extracting_furnace_gui.png");
@@ -27,10 +39,30 @@ public class OdourExtractingFurnaceRecipeCategory implements IRecipeCategory<Odo
     private final IDrawable background;
     List<ItemStack> list = List.of(ModItems.NAMELESS_CHARCOAL.get().getDefaultInstance(), Items.CHARCOAL.getDefaultInstance());
     private final IDrawable icon;
+    private final LoadingCache<Integer, IDrawableAnimated> cachedFlames;
+    private final LoadingCache<Integer, IDrawableAnimated> cachedProgressArrow;
 
     public OdourExtractingFurnaceRecipeCategory(IGuiHelper helper) {
         this.background = helper.createDrawable(TEXTURES,0,0,176,80);
         this.icon = helper.createDrawableIngredient(VanillaTypes.ITEM_STACK, new ItemStack(ModBlocks.ODOUR_EXTRACTING_FURNACE.get()));
+        this.cachedFlames = CacheBuilder.newBuilder()
+                .maximumSize(27)
+                .build(new CacheLoader<>() {
+                    @Override
+                    public IDrawableAnimated load(Integer burnTime) {
+                        return helper.drawableBuilder(TEXTURES, 176, 0, 14, 12)
+                                .buildAnimated(burnTime, IDrawableAnimated.StartDirection.TOP, true);
+                    }
+                });
+        this.cachedProgressArrow = CacheBuilder.newBuilder()
+                .maximumSize(30)
+                .build(new CacheLoader<>() {
+                    @Override
+                    public IDrawableAnimated load(Integer burnTime) {
+                        return helper.drawableBuilder(TEXTURES, 176, 14, 26, 16)
+                                .buildAnimated(burnTime, IDrawableAnimated.StartDirection.LEFT, false);
+                    }
+                });
     }
 
     @Override
@@ -62,5 +94,16 @@ public class OdourExtractingFurnaceRecipeCategory implements IRecipeCategory<Odo
         builder.addSlot(RecipeIngredientRole.OUTPUT,118,53).addItemStack(recipe.getAdditionalItem(null));
         builder.addSlot(RecipeIngredientRole.OUTPUT,118,21).addItemStack(recipe.getResultItem(null));
 
+    }
+
+    @Override
+    public void draw(OdourExtractingRecipe recipe, IRecipeSlotsView recipeSlotsView, GuiGraphics guiGraphics, double mouseX, double mouseY) {
+        IRecipeCategory.super.draw(recipe, recipeSlotsView, guiGraphics, mouseX, mouseY);
+        int burnTime = 200;
+        int ProgressArrowTime = 100;
+        IDrawableAnimated flame = cachedFlames.getUnchecked(burnTime);
+        flame.draw(guiGraphics, 56, 37);
+        IDrawableAnimated progress_arrow = cachedProgressArrow.getUnchecked(ProgressArrowTime);
+        progress_arrow.draw(guiGraphics,79,20);
     }
 }
