588c67589a48fd1539f86f947af95b0c1f4d68e9
Vladimir <it@vladimirva.ru>
2024-02-09 23:08:51 +0300

    nameless tree fence gate recipe added

--- src/main/java/net/sfedu/ars_maleficarum/datagen/ModRecipeProvider.java
+++ src/main/java/net/sfedu/ars_maleficarum/datagen/ModRecipeProvider.java
@@ -442,6 +442,14 @@ public class ModRecipeProvider extends RecipeProvider implements IConditionBuild
                 .unlockedBy("has_rowan_planks", has(ModBlocks.ROWAN_PLANKS.get()))
                 .save(pWriter);
 
+        // Крафт ворот из безымянного дерева
+        ShapedRecipeBuilder.shaped(RecipeCategory.MISC, ModBlocks.NAMELESS_TREE_FENCE_GATE.get())
+                .pattern("SPS")
+                .pattern("SPS")
+                .define('P', ModBlocks.NAMELESS_TREE_PLANKS.get())
+                .define('S', Items.STICK)
+                .unlockedBy("has_nameless_tree_planks", has(ModBlocks.NAMELESS_TREE_PLANKS.get()))
+                .save(pWriter);
 
 
         //Генерация крафтов в новой печке
