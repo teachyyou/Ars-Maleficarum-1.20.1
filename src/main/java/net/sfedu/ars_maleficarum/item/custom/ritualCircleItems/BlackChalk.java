1e01db539257a202fb8351950cb933dead37df16
teachyyou <kudriavtsiev_2004@mail.ru>
2024-04-14 03:33:22 +0300

    Improved Chalks Behaviour

--- src/main/java/net/sfedu/ars_maleficarum/item/custom/ritualCircleItems/BlackChalk.java
+++ src/main/java/net/sfedu/ars_maleficarum/item/custom/ritualCircleItems/BlackChalk.java
@@ -6,57 +6,19 @@ import net.minecraft.world.InteractionResult;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.context.UseOnContext;
+import net.minecraft.world.level.block.Blocks;
 import net.sfedu.ars_maleficarum.block.ModBlocks;
 import net.sfedu.ars_maleficarum.block.custom.chalkSymbols.ChalkSymbol;
 import net.sfedu.ars_maleficarum.sound.ModSounds;
 
 import java.util.Random;
 
-public class BlackChalk extends Item {
+public class BlackChalk extends ChalkItem {
 
     public BlackChalk(Properties pProperties) {
         super(pProperties);
+        //todo переделать
+        chalkSymbol = ModBlocks.WHITE_CHALK_SYMBOL.get();
     }
-
-    @Override // TODO Переделать когда добавятся черные символы
-    public InteractionResult useOn(UseOnContext pContext) {
-//        if (!pContext.getLevel().isClientSide()) {
-//            Direction[] dirs = new Direction[] {Direction.NORTH, Direction.SOUTH, Direction.EAST, Direction.WEST};
-//            Direction glyphDirection = dirs[new Random().nextInt(4)];
-//            if (pContext.getLevel().getBlockState(pContext.getClickedPos()).is(ModBlocks.GREEN_CHALK_SYMBOL.get())) {
-//                int variant = pContext.getLevel().getBlockState(pContext.getClickedPos()).getValue(ChalkSymbol.VARIANT);
-//                pContext.getLevel().setBlock(pContext.getClickedPos(), ModBlocks.GREEN_CHALK_SYMBOL.get().defaultBlockState().setValue(ChalkSymbol.VARIANT,(variant+1)%11).setValue(ChalkSymbol.FACING, glyphDirection),3);
-//            }
-//            else if (pContext.getLevel().getBlockState(pContext.getClickedPos()).isCollisionShapeFullBlock(pContext.getLevel(),pContext.getClickedPos())) {
-//                Random random = new Random();
-//                pContext.getLevel().setBlock(pContext.getClickedPos().above(), ModBlocks.GREEN_CHALK_SYMBOL.get().defaultBlockState().setValue(ChalkSymbol.VARIANT,random.nextInt(11 )).setValue(ChalkSymbol.FACING, glyphDirection),3);
-//            }
-//            else {
-//                return InteractionResult.FAIL;
-//            }
-//        }
-//        pContext.getItemInHand().hurtAndBreak(1,pContext.getPlayer(),
-//                player -> player.broadcastBreakEvent(player.getUsedItemHand()));
-//        pContext.getLevel().playSound(null,pContext.getClickedPos(), ModSounds.CHALK_USE.get(), SoundSource.PLAYERS);
-//        return InteractionResult.SUCCESS;
-        return InteractionResult.FAIL;
-    }
-
-
-    @Override
-    public int getMaxDamage(ItemStack stack) {
-        return 44;
-    }
-
-    @Override
-    public int getMaxStackSize(ItemStack stack) {
-        return 1;
-    }
-
-    @Override
-    public boolean isDamageable(ItemStack stack) {
-        return true;
-    }
-
 
 }
--- src/main/java/net/sfedu/ars_maleficarum/item/custom/ritualCircleItems/CrimsonChalk.java
+++ src/main/java/net/sfedu/ars_maleficarum/item/custom/ritualCircleItems/CrimsonChalk.java
@@ -12,50 +12,11 @@ import net.sfedu.ars_maleficarum.sound.ModSounds;
 
 import java.util.Random;
 
-public class CrimsonChalk extends Item {
+public class CrimsonChalk extends ChalkItem {
 
     public CrimsonChalk(Properties pProperties) {
         super(pProperties);
+        chalkSymbol = ModBlocks.CRIMSON_CHALK_SYMBOL.get();
     }
-
-    @Override
-    public InteractionResult useOn(UseOnContext pContext) {
-        if (!pContext.getLevel().isClientSide()) {
-            Direction[] dirs = new Direction[] {Direction.NORTH, Direction.SOUTH, Direction.EAST, Direction.WEST};
-            Direction glyphDirection = dirs[new Random().nextInt(4)];
-            if (pContext.getLevel().getBlockState(pContext.getClickedPos()).is(ModBlocks.CRIMSON_CHALK_SYMBOL.get())) {
-                int variant = pContext.getLevel().getBlockState(pContext.getClickedPos()).getValue(ChalkSymbol.VARIANT);
-                pContext.getLevel().setBlock(pContext.getClickedPos(), ModBlocks.CRIMSON_CHALK_SYMBOL.get().defaultBlockState().setValue(ChalkSymbol.VARIANT,(variant+1)%11).setValue(ChalkSymbol.FACING, glyphDirection),3);
-            }
-            else if (pContext.getLevel().getBlockState(pContext.getClickedPos()).isCollisionShapeFullBlock(pContext.getLevel(),pContext.getClickedPos())) {
-                Random random = new Random();
-                pContext.getLevel().setBlock(pContext.getClickedPos().above(), ModBlocks.CRIMSON_CHALK_SYMBOL.get().defaultBlockState().setValue(ChalkSymbol.VARIANT,random.nextInt(11 )).setValue(ChalkSymbol.FACING, glyphDirection),3);
-            }
-            else {
-                return InteractionResult.FAIL;
-            }
-        }
-        pContext.getItemInHand().hurtAndBreak(1,pContext.getPlayer(),
-                p->{});
-        pContext.getLevel().playSound(null,pContext.getClickedPos(), ModSounds.CHALK_USE.get(), SoundSource.PLAYERS);
-        return InteractionResult.SUCCESS;
-    }
-
-
-    @Override
-    public int getMaxDamage(ItemStack stack) {
-        return 44;
-    }
-
-    @Override
-    public int getMaxStackSize(ItemStack stack) {
-        return 1;
-    }
-
-    @Override
-    public boolean isDamageable(ItemStack stack) {
-        return true;
-    }
-
 
 }
