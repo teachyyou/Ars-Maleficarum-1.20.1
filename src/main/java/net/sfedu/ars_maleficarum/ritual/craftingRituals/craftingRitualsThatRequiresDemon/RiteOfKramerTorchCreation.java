f575bd1dd178e7a421425a849ee028b47c99b1fa
teachyyou <kudriavtsiev_2004@mail.ru>
2024-04-24 20:52:22 +0300

    Prepared Kramer's Torch Creation Ritual base

--- src/main/java/net/sfedu/ars_maleficarum/ritual/craftingRituals/craftingRitualsThatRequiresDemon/RiteOfKramerTorchCreation.java
+++ src/main/java/net/sfedu/ars_maleficarum/ritual/craftingRituals/craftingRitualsThatRequiresDemon/RiteOfKramerTorchCreation.java
@@ -9,7 +9,9 @@ import net.minecraft.sounds.SoundEvents;
 import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LightningBolt;
+import net.minecraft.world.entity.MobSpawnType;
 import net.minecraft.world.entity.item.ItemEntity;
+import net.minecraft.world.entity.monster.Monster;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
@@ -23,6 +25,7 @@ import net.sfedu.ars_maleficarum.item.ModItems;
 import net.sfedu.ars_maleficarum.ritual.ritualTemplates.CircleRitual;
 import net.sfedu.ars_maleficarum.sound.ModSounds;
 
+import java.util.NoSuchElementException;
 import java.util.Optional;
 
 public class RiteOfKramerTorchCreation extends CircleRitual {
@@ -56,35 +59,45 @@ public class RiteOfKramerTorchCreation extends CircleRitual {
         consumeComponents(pLevel,pPos,riteCore, pPlayer);
         ticks++;
 
-        if (allComponentsConsumed && ticks%20==0) {
+        if (allComponentsConsumed && ticks%5==0) {
             double d0 = staff.position().x;
             double d1 = staff.position().y;
             double d2 = staff.position().z;
-            ((ServerLevel)pLevel).sendParticles(ParticleTypes.FLAME, d0, d1, d2, 100, 0,0.5D,0,0.2);
-            ((ServerLevel)pLevel).sendParticles(ParticleTypes.FLAME, d0, d1, d2, 100, 0,0.1D,0,0.2);
+
+            ((ServerLevel)pLevel).sendParticles(ParticleTypes.FLAME, d0, d1, d2, 100, 0,0.5D,0,0.01);
+            ((ServerLevel)pLevel).sendParticles(ParticleTypes.FLAME, d0, d1, d2, 100, 0,1D,0,0.01);
+
+            if (ticks%20==0) pLevel.playSound(null, pPos,itemConsumeSound, SoundSource.HOSTILE,1F,1F);
         }
-        if (allComponentsConsumed && ticks/20==5) {
-            Optional<GluttonyDemonEntity> possiblyDemon = pLevel.getEntitiesOfClass(GluttonyDemonEntity.class,new AABB(pPos.relative(Direction.Axis.Z,-3).relative(Direction.Axis.X,-3).relative(Direction.Axis.Y,-5),pPos.relative(Direction.Axis.Z,3).relative(Direction.Axis.X,3).relative(Direction.Axis.Y,5)))
+
+        if (allComponentsConsumed && ticks/20.0==5.0) {
+            Optional<GluttonyDemonEntity> possiblyDemon = pLevel.getEntitiesOfClass(GluttonyDemonEntity.class ,new AABB(pPos.relative(Direction.Axis.Z,-3).relative(Direction.Axis.X,-3).relative(Direction.Axis.Y,-5),pPos.relative(Direction.Axis.Z,3).relative(Direction.Axis.X,3).relative(Direction.Axis.Y,5)))
                     .stream().findAny();
             if (possiblyDemon.isEmpty()) {
-                pPlayer.thunderHit((ServerLevel) pLevel,new LightningBolt(EntityType.LIGHTNING_BOLT,pLevel)); //todo проверить работает ли, выглядит как дичь
+                EntityType.LIGHTNING_BOLT.spawn((ServerLevel) pLevel,null,pPlayer,pPlayer.blockPosition(), MobSpawnType.TRIGGERED,true,true);
+                //pPlayer.thunderHit((ServerLevel) pLevel,new LightningBolt(EntityType.LIGHTNING_BOLT,pLevel)); //todo проверить работает ли, выглядит как дичь
+                pLevel.playSound(null, pPos, ModSounds.MYSTIC_WHISPERING.get(), SoundSource.HOSTILE,1F,1F);
+
                 pPlayer.kill();
                 riteCore.stopRitual();
                 return;
             }
             GluttonyDemonEntity demon = possiblyDemon.get();
+
             double d0 = demon.position().x;
             double d1 = demon.position().y;
             double d2 = demon.position().z;
 
-            ((ServerLevel)pLevel).sendParticles(ParticleTypes.ENCHANT, d0, d1, d2, 100, 0,0.5D,0,0.2);
-            ((ServerLevel)pLevel).sendParticles(ParticleTypes.ENCHANT, d0, d1, d2, 100, 0,1D,0,0.2);
+            ((ServerLevel)pLevel).sendParticles(ParticleTypes.ENCHANT, d0, d1, d2, 100, 0,0.5D,0,0.5);
+            ((ServerLevel)pLevel).sendParticles(ParticleTypes.ENCHANT, d0, d1, d2, 100, 0,1D,0,0.5);
+            ((ServerLevel)pLevel).sendParticles(ParticleTypes.ENCHANT, d0, d1, d2, 100, 0,1.5D,0,0.5);
+            ((ServerLevel)pLevel).sendParticles(ParticleTypes.ENCHANT, d0, d1, d2, 100, 0,2D,0,0.5);
             pLevel.playSound(null, pPos, ModSounds.GLUTTONY_DEMON_DIED.get(), SoundSource.HOSTILE,1F,1F);
-            possiblyDemon.get().discard();
+            demon.discard();
         }
 
 
-        if (allComponentsConsumed && ticks/20.0 == 6) {
+        if (allComponentsConsumed && ticks/20.0 == 6.0) {
             double d0 = staff.position().x;
             double d1 = staff.position().y;
             double d2 = staff.position().z;
@@ -92,7 +105,7 @@ public class RiteOfKramerTorchCreation extends CircleRitual {
             ((ServerLevel)pLevel).sendParticles(ParticleTypes.FLAME, d0, d1, d2, 100, 0,0.5D,0,0.2);
             ((ServerLevel)pLevel).sendParticles(ParticleTypes.FLAME, d0, d1, d2, 100, 0,1D,0,0.2);
             pLevel.playSound(null, pPos, SoundEvents.WITHER_DEATH, SoundSource.PLAYERS,1F,1F);
-            staff.setItem(new ItemStack(ModItems.SILVER_DAGGER.get())); //TODO Поместить сюда потом огненный посох
+            staff.setItem(new ItemStack(ModItems.POISON_STAFF.get())); //TODO Поместить сюда потом огненный посох
             pPlayer.sendSystemMessage(Component.translatable(ritualName));
             ticks=0;
             riteCore.stopRitual();
