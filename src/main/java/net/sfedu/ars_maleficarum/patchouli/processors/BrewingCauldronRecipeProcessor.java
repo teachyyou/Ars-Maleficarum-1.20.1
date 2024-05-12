1675c3f1127f6a7dbd15ae0df54164d6df45322f
teachyyou <kudriavtsiev_2004@mail.ru>
2024-05-12 11:31:27 +0300

    Working on proper cauldron recipe ingredients display

--- src/main/java/net/sfedu/ars_maleficarum/patchouli/processors/BrewingCauldronRecipeProcessor.java
+++ src/main/java/net/sfedu/ars_maleficarum/patchouli/processors/BrewingCauldronRecipeProcessor.java
@@ -16,24 +16,48 @@ import vazkii.patchouli.api.IVariable;
 import vazkii.patchouli.api.IVariableProvider;
 
 import java.sql.Array;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class BrewingCauldronRecipeProcessor implements IComponentProcessor {
     private BrewingCauldronRecipe recipe;
-
     private List<ItemStack> input;
     private ItemStack resultItem;
     private ItemStack collectItem;
+    int[] itemsOrder;
+    private int itemIndex = 0;
+
+    private void setupOrderArray(int count) {
+        itemsOrder = switch (count) {
+            case 1 -> new int[] {0,0,0,0,0,0,0,1,0,0};
+            case 2 -> new int[] {0,0,0,0,0,0,1,0,1,0};
+            case 3 -> new int[] {0,0,0,0,0,0,1,1,1,0};
+            case 4 -> new int[] {0,0,0,0,0,1,1,1,1,0};
+            case 5 -> new int[] {0,0,0,0,0,1,1,1,1,1};
+            case 6 -> new int[] {0,1,1,1,0,0,1,1,1,0};
+            case 7 -> new int[] {0,1,0,1,0,1,1,1,1,1};
+            case 8 -> new int[] {0,1,1,1,0,1,1,1,1,1};
+            case 9 -> new int[] {1,1,0,1,1,1,1,1,1,1};
+            case 10 -> new int[] {1,1,1,1,1,1,1,1,1,1};
+            default -> new int[]{};
+        };
+    }
 
     @Override
     public void setup(Level level, IVariableProvider iVariableProvider) {
+
+
+
         ResourceLocation recipeId = new ResourceLocation(iVariableProvider.get("recipe").asString());
 
         RecipeManager recipeManager = level.getRecipeManager();
         recipe = (BrewingCauldronRecipe) recipeManager.byKey(recipeId).orElseThrow(()->new IllegalArgumentException("Could not find recipe for: " + recipeId));
-        input = recipe.getIngredients().stream().map(x-> x.getItems().length>0 ? x.getItems()[0] : ItemStack.EMPTY).toList();
+        //todo check for empty condition (might be buggy)
+        input = recipe.getIngredients().stream().filter(x->!x.isEmpty()).map(x-> x.getItems()[0]).toList();
+        int count = input.size();
+        setupOrderArray(count);
         resultItem = recipe.getResultItem(null);
 
         collectItem = switch (recipe.craftType) {
@@ -50,8 +74,8 @@ public class BrewingCauldronRecipeProcessor implements IComponentProcessor {
     public IVariable process(Level level, String key) {
         if(key.startsWith("input")) {
             int index = key.charAt(key.length()-1) - '0';
-            if (index < input.size()) {
-                return IVariable.from(input.get(index));
+            if (itemsOrder[index]==1) {
+                return IVariable.from(input.get(itemIndex++));
             }
             return IVariable.empty();
         }
